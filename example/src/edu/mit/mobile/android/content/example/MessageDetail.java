ff01fe335ead73140b6a1da5db7a4dc583406008
Steve Pomeroy <steve@staticfree.info>
2012-04-11 14:59:23 -0400

    Adds action bar options to MessageDetail

--- example/src/edu/mit/mobile/android/content/example/MessageDetail.java
+++ example/src/edu/mit/mobile/android/content/example/MessageDetail.java
@@ -1,12 +1,18 @@
 package edu.mit.mobile.android.content.example;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,8 +26,10 @@ import android.widget.Toast;
 public class MessageDetail extends Activity {
 	private TextView mTitle, mBody, mDate;
 
-	private static final String[] PROJECTION = { Message._ID, Message.TITLE,
-			Message.BODY, Message.CREATED_DATE };
+	private static final String[] PROJECTION = { Message._ID, Message.TITLE, Message.BODY,
+			Message.CREATED_DATE };
+
+	private static final int DIALOG_CONFIRM_DELETE = 100;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +59,54 @@ public class MessageDetail extends Activity {
 			Toast.makeText(
 					this,
 					MessageDetail.class.getSimpleName()
-							+ " doesn't know how to handle the intent: "
-							+ intent, Toast.LENGTH_LONG).show();
+							+ " doesn't know how to handle the intent: " + intent,
+					Toast.LENGTH_LONG).show();
 			finish();
 		}
-
 	}
 
-	private Cursor queryDatabase(Uri message){
-		final Cursor c = managedQuery(message, PROJECTION, null,
-				null, null);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// the item context menu can be easily reused for a detail action bar
+		getMenuInflater().inflate(R.menu.item_context, menu);
+
+		// this is the view screen, so we hide this.
+		menu.findItem(R.id.view).setVisible(false);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.edit:
+				startActivity(new Intent(Intent.ACTION_EDIT, getIntent().getData()));
+				break;
+
+			case R.id.delete:
+				showDialog(DIALOG_CONFIRM_DELETE);
+				break;
+
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+			case DIALOG_CONFIRM_DELETE:
+				return new AlertDialog.Builder(this).setCancelable(true)
+						.setPositiveButton(R.string.delete, mDeleteDialogListener)
+						.setNegativeButton(android.R.string.cancel, mDeleteDialogListener).create();
+			default:
+				return super.onCreateDialog(id);
+		}
+	}
+
+	private Cursor queryDatabase(Uri message) {
+		@SuppressWarnings("deprecation")
+		final Cursor c = managedQuery(message, PROJECTION, null, null, null);
 		return c;
 	}
 
@@ -93,10 +139,8 @@ public class MessageDetail extends Activity {
 					// DateUtils.
 					+ DateUtils.formatDateTime(this,
 							c.getLong(c.getColumnIndex(Message.CREATED_DATE)),
-							DateUtils.FORMAT_SHOW_DATE
-									| DateUtils.FORMAT_SHOW_TIME
-									| DateUtils.FORMAT_SHOW_WEEKDAY
-									| DateUtils.FORMAT_SHOW_YEAR));
+							DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_TIME
+									| DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_YEAR));
 		} else {
 			// If moveToFirst didn't return true, that means our URI no longer
 			// resolves. The most common reason for this is that the content was
@@ -105,9 +149,14 @@ public class MessageDetail extends Activity {
 		}
 	}
 
+	private void deleteItem() {
+		getContentResolver().delete(getIntent().getData(), null, null);
+		finish();
+	}
+
 	/**
-	 * A background thread that shows an indeterminate progress bar in the window title
-	 * while the content is loading.
+	 * A background thread that shows an indeterminate progress bar in the window title while the
+	 * content is loading.
 	 *
 	 */
 	private class ContentLoadTask extends AsyncTask<Uri, Void, Cursor> {
@@ -129,4 +178,16 @@ public class MessageDetail extends Activity {
 			setProgressBarIndeterminateVisibility(false);
 		}
 	}
+
+	private final OnClickListener mDeleteDialogListener = new OnClickListener() {
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			switch (which) {
+				case DialogInterface.BUTTON_POSITIVE:
+					deleteItem();
+					break;
+			}
+		}
+	};
 }
